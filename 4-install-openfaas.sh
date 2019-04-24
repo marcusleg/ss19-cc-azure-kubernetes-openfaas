7abc74b3fb5d9e18e91fa4920a90fbf5966d4074
Marcus Legendre <marcus.legendre@gmail.com>
2019-04-24 18:45:05 +0200

    Scripts for installing helm and OpenFaaS

---
+++ 4-install-openfaas.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# create namespaces
+kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
+
+# add helm repo for OpenFaaS 
+helm repo add openfaas https://openfaas.github.io/faas-netes/
+
+# generate password
+PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
+kubectl -n openfaas create secret generic basic-auth \
+--from-literal=basic-auth-user=admin \
+--from-literal=basic-auth-password="$PASSWORD"
+echo "Username: admin\nPassword: ${PASSWORD}" > artefacts/openfaas-login.txt
+
+# install OpenFaaS helm chart
+helm repo update \
+&& helm upgrade openfaas --install openfaas/openfaas \
+--namespace openfaas  \
+--set basic_auth=true \
+--set functionNamespace=openfaas-fn \
+--set serviceType=LoadBalancer
