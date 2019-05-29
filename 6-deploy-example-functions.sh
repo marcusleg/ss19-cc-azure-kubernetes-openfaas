c28120230a147f7bed693445d0926f2f16e60c17
Marcus Legendre <marcus.legendre@gmail.com>
2019-05-29 06:25:52 +0200

    Additional script that deploys larikk's example functions

---
+++ 6-deploy-example-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export OPENFAAS_URL="http://$(helm status openfaas | grep gateway-external | awk '{print $4}'):8080"
+
+faas-cli deploy --image=larikk/hello-user --name=hello-user --handler=./hello-user --lang python3
+faas-cli deploy --image=larikk/hostname --name=hostname --handler=./hostname --lang python3
+faas-cli deploy --image=larikk/mandelbrot --name=mandelbrot --handler=./mandelbrot --lang java8
+
+echo -n "Testing invokation of function 'hello-user'. Response: "
+curl --data '{"name": "Kevin", "birthdate": "1990-12-24"}' http://40.115.106.38:8080/function/hello-user
+
+echo "To invoke the 'mandelbrot' function please visit ${OPENFAAS_URL}/function/mandelbrot?w=1600&h=1200&iter=40&html=true"
