2e5273a0b5b5e662d6a1c1dc1c16adbb1f3b52c1
Marcus Legendre <marcus.legendre@gmail.com>
2019-06-03 13:29:55 +0200

    Forgot a hardcoded URL

--- 6-deploy-example-functions.sh
+++ 6-deploy-example-functions.sh
@@ -6,6 +6,6 @@ faas-cli deploy --image=larikk/hostname --name=hostname --handler=./hostname --l
 faas-cli deploy --image=larikk/mandelbrot --name=mandelbrot --handler=./mandelbrot --lang java8
 
 echo -n "Testing invokation of function 'hello-user'. Response: "
-curl --data '{"name": "Kevin", "birthdate": "1990-12-24"}' http://40.115.106.38:8080/function/hello-user
+curl --data '{"name": "Kevin", "birthdate": "1990-12-24"}' ${OPENFAAS_URL}/function/hello-user
 
 echo "To invoke the 'mandelbrot' function please visit ${OPENFAAS_URL}/function/mandelbrot?w=1600&h=1200&iter=40&html=true"
